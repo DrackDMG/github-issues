23873c304b0b5f6a9e4354c24990e1f0217d7b22
DrackDMG <drackmg@gmail.com>
2025-10-04 13:03:32 -0600

    realizar peticion http

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { IssuesService } from '../../services/issues.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-issues-list-page',
-  imports: [RouterLink],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './issues-list-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class IssuesListPageComponent { }
+export default class IssuesListPageComponent {
+  issuesService = inject(IssuesService);
+
+  get labelsQuery() {
+    return this.issuesService.labelsQuery;
+  }
+}
