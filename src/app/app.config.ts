cb68fcb2da645f5da279b88a45f0d5ec48ffb689
DrackDMG <drackmg@gmail.com>
2025-10-04 12:32:20 -0600

    instalacion tankstack query

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { withDevtools } from '@tanstack/angular-query-experimental/devtools'
+
+
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental'
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideTanStackQuery(new QueryClient(), withDevtools())
   ]
 };
